5188a6c7e0c84acff47e7d60c7dbaafb1a36afc0
Anton Fomenko <antonfomenko1234@gmail.com>
2024-08-14 19:41:40 +0300

    finaly\

--- src/widget.py
+++ src/widget.py
@@ -25,17 +25,16 @@ def get_data(time_data: str) -> str:
     return new_time_data
 
 
-data = ['Maestro 1596837868705199',
-        'Счет 64686473678894779589',
-        'MasterCard 7158300734726758',
-        'Счет 35383033474447895560',
-        'Visa Classic 6831982476737658',
-        'Visa Platinum 8990922113665229',
-        'Visa Gold 5999414228426353',
-        'Счет 73654108430135874305']
-
 if __name__ == '__main__':
-    for i in data:
+    data_tests = ['Maestro 1596837868705199',
+                  'Счет 64686473678894779589',
+                  'MasterCard 7158300734726758',
+                  'Счет 35383033474447895560',
+                  'Visa Classic 6831982476737658',
+                  'Visa Platinum 8990922113665229',
+                  'Visa Gold 5999414228426353',
+                  'Счет 73654108430135874305']
+    for i in data_tests:
         print(mask_account_card(i))
 
     print(get_data('2024-03-11T02:26:18.671407'))
