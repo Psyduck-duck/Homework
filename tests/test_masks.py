c56423861f6f12dd000352d23662302b0fec17f4
Anton Fomenko <antonfomenko1234@gmail.com>
2024-08-22 12:36:37 +0300

    lint fixes

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,12 +1,13 @@
 import pytest
-from src.masks import get_mask_card_number, get_mask_account
+
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def test_get_mask_card_number():
-    assert get_mask_card_number(1234567890123456) == '1234 56** **** 3456'
+    assert get_mask_card_number(1234567890123456) == "1234 56** **** 3456"
 
 
-@pytest.mark.parametrize('number', [(1234), (123456780321565465432165), (-1)])
+@pytest.mark.parametrize("number", [(1234), (123456780321565465432165), (-1)])
 def test_get_mask_card_number_invalid_number(number):
     with pytest.raises(ValueError):
         get_mask_card_number(number)
@@ -14,16 +15,16 @@ def test_get_mask_card_number_invalid_number(number):
 
 def test_get_mask_card_number_invalid_type():
     with pytest.raises(TypeError):
-        get_mask_card_number('номер карты')
+        get_mask_card_number("номер карты")
 
 
 def test_get_mask_account():
-    assert get_mask_account(12345678901234567890) == '**7890'
+    assert get_mask_account(12345678901234567890) == "**7890"
 
 
 def test_get_mask_invalid_type_account():
     with pytest.raises(TypeError):
-        get_mask_account('Hello')
+        get_mask_account("Hello")
 
 
 def test_get_mask_account_invalid_len():
