f63e149f23e6c25df14a155d8d20f7e7e6fdd9b7
Anton Fomenko <antonfomenko1234@gmail.com>
2024-09-12 16:20:04 +0300

    add get_amount_transaction func

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,12 @@
 import os
 import json
 
+from external_api import converte_currency
 
 
 def get_operations_data(filename: str) -> list:
-    """ Функция принимает название файла в дирректории Data и возвращает данные"""
+    """Функция принимает название файла в дирректории Data и возвращает данные"""
+
     with open(f"data/{filename}", "r", encoding="utf-8") as file:
         try:
             operations_data = json.load(file)
@@ -17,14 +19,14 @@ def get_operations_data(filename: str) -> list:
 
 
 def get_amount_transaction(id: int) -> float:
-    operations_data = get_operations_data("data/operations.json")
+    """Фунция принимает транзакцию и возвращает сумму транзакции в рублях"""
+
+    operations_data = get_operations_data("operations.json")
     for operation in operations_data:
         if operation["id"] == id:
             amount = operation["operationAmount"]["amount"]
             currency_code = operation["operationAmount"]["currency"]["code"]
             if currency_code != "RUB":
-
-
-
-if __name__ == "__main__":
-    print(get_operations_data("operations.json"))
+                result = converte_currency(currency_code, "RUB", amount)
+                return result
+            return amount
