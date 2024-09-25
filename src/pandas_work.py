4ee9a3779a9740e9831f59dbd36ddfa472ff5295
Anton Fomenko <antonfomenko1234@gmail.com>
2024-09-25 21:40:09 +0300

    fixes

--- src/pandas_work.py
+++ src/pandas_work.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 def read_csv_file(path_to_file: str) -> list[dict]:
     """Обработка csv файла в список словарей, указать путь к файлу"""
-    df = pd.read_csv(f"{path_to_file}")
+    df = pd.read_csv(f"{path_to_file}", sep=";")
     df_dict = df.to_dict(orient="records")
     return df_dict
 
@@ -16,5 +16,5 @@ def read_excel_file(path_to_file: str) -> list[dict]:
     df_dict = df.to_dict(orient="records")
     return df_dict
 
-
-# print(read_csv_file("../data/transactions.csv"))
+#print(read_csv_file("../data/transactions.csv"))
+#print(read_excel_file("../data/transactions_excel.xlsx"))
